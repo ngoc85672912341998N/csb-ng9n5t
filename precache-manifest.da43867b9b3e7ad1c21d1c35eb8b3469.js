1bd97db634f99ec40b96d688a94a106dcf8439c1
Code Sandbox <hello@codesandbox.io>
2022-08-03 03:20:28 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.da43867b9b3e7ad1c21d1c35eb8b3469.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "957c774f398ce5a041bc51ff23125f98",
+    "url": "https://ngoc85672912341998n.github.io/csb-ng9n5t/index.html"
+  },
+  {
+    "revision": "40aa35ba629989f4c41a",
+    "url": "https://ngoc85672912341998n.github.io/csb-ng9n5t/static/js/2.63fc25ca.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "https://ngoc85672912341998n.github.io/csb-ng9n5t/static/js/2.63fc25ca.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "952fc7848caeac9a600a",
+    "url": "https://ngoc85672912341998n.github.io/csb-ng9n5t/static/js/main.9e9142cc.chunk.js"
+  },
+  {
+    "revision": "4d2fb9e37461bb2cb3a5",
+    "url": "https://ngoc85672912341998n.github.io/csb-ng9n5t/static/js/runtime-main.a5b91acd.js"
+  }
+]);
